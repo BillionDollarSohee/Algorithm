10afe533779e651cfefc8983edae81fe7bf76491
KimSoHee <118681718+BillionDollarSohee@users.noreply.github.com>
2023-09-15 16:29:08 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.35 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,17 +1,10 @@
+import java.util.Arrays;
 class Solution {
     public int solution(int[] numbers) {
-        int firstMax = 0;
-        int secondMax = 0;
-        
-        for (int i = 0; i < numbers.length; i++) {
-            if (firstMax < numbers[i]) {
-                secondMax = firstMax;
-                firstMax = numbers[i];
-            } else if (secondMax < numbers[i]) {
-                secondMax = numbers[i];
-            }
-        }
-        
-        return firstMax * secondMax;
+        Arrays.sort(numbers);
+        int firstMaxValue = numbers[numbers.length - 1];
+        int secondMaxValue = numbers[numbers.length - 2];
+
+        return firstMaxValue * secondMaxValue;
     }
 }
