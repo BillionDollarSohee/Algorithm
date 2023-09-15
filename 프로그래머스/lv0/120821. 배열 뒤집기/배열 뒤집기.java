22a8d2ad0d5d6f5c72b97b63b26082dbd55e1b9d
KimSoHee <118681718+BillionDollarSohee@users.noreply.github.com>
2023-09-15 16:09:00 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length];
-        int num = 0;
-        for (int i = num_list.length - 1; i >= 0; i--) {
-            answer[num] = num_list[i];
-            num++;
+        
+        for(int i = 0; i< num_list.length; i++){
+            answer[i] = num_list[num_list.length-i-1];
         }
+        
         return answer;
     }
 }
