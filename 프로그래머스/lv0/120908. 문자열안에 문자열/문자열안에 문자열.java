edeb98e6dbae2f1e5e89b95a6adb8834eb54fc51
KimSoHee <118681718+BillionDollarSohee@users.noreply.github.com>
2023-09-16 17:25:11 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,26 +1,8 @@
 class Solution {
     public int solution(String str1, String str2) {
-        int answer = 2;
-
-        char[] arrStr1 = str1.toCharArray();
-        char first = str2.charAt(0);
-        
-        for (int i = 0; i < arrStr1.length; i++) {
-            if (arrStr1[i] == first) {
-                boolean is = true;
-                for (int j = 1; j < str2.length(); j++) {
-                    if (i + j >= arrStr1.length || arrStr1[i + j] != str2.charAt(j)) {
-                        is = false;
-                        break;
-                    }
-                }
-                if (is) {
-                    answer = 1; 
-                    break; 
-                }
-            }
+        if (str1.contains(str2)) {
+            return 1;
         }
-        
-        return answer;
+        return 2;
     }
 }
