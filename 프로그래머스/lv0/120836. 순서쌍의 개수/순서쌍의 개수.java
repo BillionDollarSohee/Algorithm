f01263c6346444135878005c9c47af22172da29e
KimSoHee <118681718+BillionDollarSohee@users.noreply.github.com>
2023-09-18 12:51:01 +0900

    [level 0] Title: 순서쌍의 개수, Time: 14.91 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -3,12 +3,7 @@ class Solution {
         int answer = 0;
         for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
-                int divisor1 = i;
-                int divisor2 = n / i;
-                
-                if (divisor1 * divisor2 == n) {
-                    answer += 1;
-                }
+                answer += 1;
             }
         }
         
