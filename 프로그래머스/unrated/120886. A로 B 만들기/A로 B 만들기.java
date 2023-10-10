8525e208bccd6840dff79776d71d6b3950e96eb5
KimSoHee <118681718+BillionDollarSohee@users.noreply.github.com>
2023-10-11 01:28:22 +0900

    [level unrated] Title: A로 B 만들기, Time: 0.30 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,22 @@
+
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String before, String after) {
+        int answer = 1;
+        
+        String[] beforeArr = before.split("");
+        String[] afterArr = after.split("");
+        
+        Arrays.sort(beforeArr);
+        Arrays.sort(afterArr);
+        
+        for (int i = 0; i < beforeArr.length; i++) {
+            if (!beforeArr[i].equals(afterArr[i])) {
+                answer = 0;
+            }
+        }
+            
+        return answer;
+    }
+}
