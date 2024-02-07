a28c96f8473562e169c35944506c3ea2a38f015a
KimSoHee <118681718+BillionDollarSohee@users.noreply.github.com>
2024-02-07 16:21:58 +0900

    [level 1] Title: 시저 암호, Time: 42.70 ms, Memory: 94.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String s, int n) {
+        String answer = "";
+    
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            
+            if( ch == ' ' ){
+                answer += " ";
+            } else if(ch >= 'A' && ch <= 'Z') {
+                answer += (char) ((ch - 'A' + n) % 26 + 'A');
+            } else if(ch >= 'a' && ch <= 'z') {
+                answer += (char) ((ch - 'a' + n) % 26 + 'a');
+            }
+        }
+        return answer;
+    }
+}
