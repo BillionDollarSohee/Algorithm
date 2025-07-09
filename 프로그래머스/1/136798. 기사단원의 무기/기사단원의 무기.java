ba780c5692a45232787b87dde49183af10133a88
KimSoHee <118681718+BillionDollarSohee@users.noreply.github.com>
2025-07-09 23:35:12 +0900

    [level 1] Title: 기사단원의 무기, Time: 93.31 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            int divisorCount = countDivisors(i); // 약수 개수 구하기
+
+            if (divisorCount > limit) {
+                answer += power;
+            } else {
+                answer += divisorCount;
+            }
+        }
+
+        return answer;
+    }
+
+    // 약수의 개수를 세는 함수
+    public int countDivisors(int n) {
+        int count = 0;
+
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                count++; // i는 약수
+                if (i != n / i) {
+                    count++; // 짝인 약수 (제곱수면 중복 제거)
+                }
+            }
+        }
+
+        return count;
+    }
+}
