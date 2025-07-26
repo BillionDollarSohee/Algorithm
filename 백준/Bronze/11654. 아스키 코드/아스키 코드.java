40647e61e0401ee9cc7efdd196032000d8ddff65
KimSoHee <118681718+BillionDollarSohee@users.noreply.github.com>
2025-07-26 23:53:26 +0900

    [Bronze V] Title: 아스키 코드, Time: 208 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.nextLine();
+
+        char ch = a.charAt(0);
+
+
+
+//        char ch = 'A';
+        int ascii = (int) ch;
+        System.out.println(ascii);
+
+
+
+    }
+}
