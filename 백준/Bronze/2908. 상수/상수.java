0d8a709dbc69a3eb559e31039d0efe6e3438c111
KimSoHee <118681718+BillionDollarSohee@users.noreply.github.com>
2025-10-23 00:22:24 +0900

    [Bronze II] Title: 상수, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,32 +1,20 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
+import java.io.*;
+import java.util.*;
+import java.util.function.IntUnaryOperator;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
-        Scanner sc = new Scanner(System.in);
+        IntUnaryOperator reverse = n -> {
+            int r = 0;
+            for (; n > 0; n /= 10) r = r * 10 + n % 10;
+            return r;
+        };
 
-        String numbers = sc.nextLine();
-
-        String[] num = numbers.split("");
-
-        String reverseNum = "";
-
-        for(int i=num.length-1; i>=0; i--){
-            reverseNum += num[i];
-        }
-
-
-
-        String[] num2 = reverseNum.split(" ");
-        int A = Integer.parseInt(num2[0]);
-        int B = Integer.parseInt(num2[1]);
-
-        if(A>B){
-            System.out.println(A);
-        }else {
-            System.out.println(B);
-        }
+        System.out.println(Math.max(reverse.applyAsInt(a), reverse.applyAsInt(b)));
     }
 }
